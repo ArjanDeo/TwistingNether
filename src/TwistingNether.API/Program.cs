0ff67d261cbfed16b688d2089d801dbb624f1e49
ArjanDeo <arjandeo@bluecomet-it.com>
2025-11-29 01:18:27 +0000

    Minor additions for old-bnet-tauri.

--- src/TwistingNether.API/Program.cs
+++ src/TwistingNether.API/Program.cs
@@ -2,10 +2,12 @@ using LazyCache;
 using Microsoft.Extensions.FileProviders;
 using Pathoschild.Http.Client;
 using System.Reflection;
+using System.Web;
 using TwistingNether.Core.Services;
 using TwistingNether.Core.Services.BattleNet;
 using TwistingNether.Core.Services.Character;
 using TwistingNether.Core.Services.WarcraftLogs;
+using TwistingNether.DataAccess.BattleNet.OAuth;
 using TwistingNether.DataAccess.Configuration;
 using Common = TwistingNether.Core.Common;
 
@@ -79,6 +81,47 @@ namespace TwistingNether.API
             app.MapControllers();
 
             app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
+
+            // Middleman required for Tauri deep linking to work with Battle.net OAuth
+            app.MapGet("/old-bnet-tauri-callback", async (HttpRequest req) =>
+            {
+               // var query = req.QueryString.HasValue ? req.QueryString.Value : "";
+              
+                string code = req.Query.FirstOrDefault(c => c.Key == "code").Value;
+                if (code == null)
+                {
+                    return Results.BadRequest("Missing code parameter");
+                }
+                 //string state = query.GetValues("state").FirstOrDefault();
+                 string clientId = app.Configuration.GetSection("OldBnetTauri-BattleNet").GetSection("ClientId").Value;
+                 string clientSecret = app.Configuration.GetSection("OldBnetTauri-BattleNet").GetSection("ClientSecret").Value;
+                try
+                {
+                    IResponse BattleNetTokenExchange = await new FluentClient()
+                    .PostAsync("https://oauth.battle.net/token")
+                    .WithArgument("redirect_uri", (app.Environment.IsDevelopment() ? "https://localhost:7176" : "https://twistingnetherapi.furyshiftz.com") + "/old-bnet-tauri-callback")
+                    .WithArgument("grant_type", "authorization_code")
+                    .WithArgument("code", code)
+                    .WithBasicAuthentication(clientId, clientSecret);
+
+                    BattleNetAccessTokenModel battleNetAccessToken = await BattleNetTokenExchange.As<BattleNetAccessTokenModel>();
+                    var @params = HttpUtility.ParseQueryString(string.Empty);
+                    @params["access_token"] = battleNetAccessToken.access_token;
+                    @params["token_type"] = battleNetAccessToken.token_type;
+                    @params["expires_in"] = battleNetAccessToken.expires_in.ToString();
+                    @params["sub"] = battleNetAccessToken.sub;
+                    @params["acquired_at"] = DateTime.UtcNow.ToString("o"); // ISO 8601 format
+
+                    var redirectUrl = $"old-bnet-tauri://callback?{@params}";
+                    return Results.Redirect(redirectUrl);
+                } catch (ApiException ex)
+                {
+                    var res = ex.Response.AsString();
+                    return Results.BadRequest(res);
+                }
+
+            });
+
 
             app.Run();
         }
